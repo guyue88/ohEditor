4e2e68b117625048ceb9e8bfc787fe5857315e3f
罗强 <luoqiang@luoqiangdeMacBook-Pro.local>
2017-11-22 11:24:02 +0800

    init

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		editor: path.resolve(__dirname, 'src/editor.js')
+	},
+	module: {
+		rules: [{
+			test: /(\.js)$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015']
+			}
+		},{
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader"
+			},{
+				loader: "css-loader"
+			},{
+				loader: "sass-loader"
+			}]
+		}]
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin()
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+};
